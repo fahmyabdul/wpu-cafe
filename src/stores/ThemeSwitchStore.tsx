ae085c5104834c6ba4a2767a6ba8897e70062aef
Fahmy Abdul <firstfahmyabdul@gmail.com>
2025-04-03 19:24:55 +0700

    feat: implement persistance ThemeSwitch store

--- src/stores/ThemeSwitchStore.tsx
+++ src/stores/ThemeSwitchStore.tsx
@@ -1,15 +1,22 @@
 import { create } from "zustand";
+import { createJSONStorage, persist } from "zustand/middleware";
 
 interface IThemeSwitch {
     isDark: boolean;
     switchTheme: (isDark: boolean) => void;
 }
 
-const useThemeSwitchStore = create<IThemeSwitch>((set) => (
-    {
-        isDark: false,
-        switchTheme: (isDark: boolean) => set(() => ({ isDark: isDark })),
-    }
-));
+const useThemeSwitchStore = create<IThemeSwitch>()(
+    persist(
+        (set) => ({
+            isDark: false,
+            switchTheme: (isDark: boolean) => set(() => ({ isDark: isDark })),
+        }),
+        {
+            name: "theme",
+            storage: createJSONStorage(()=> sessionStorage)
+        }
+    )
+)
 
 export default useThemeSwitchStore;
