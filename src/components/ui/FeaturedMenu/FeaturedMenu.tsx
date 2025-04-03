6f8b00f229344f9bc56d185dc59da5917fda1729
Fahmy Abdul <firstfahmyabdul@gmail.com>
2025-04-03 15:27:18 +0700

    feat: minor change to featured menu

--- src/components/ui/FeaturedMenu/FeaturedMenu.tsx
+++ src/components/ui/FeaturedMenu/FeaturedMenu.tsx
@@ -4,6 +4,7 @@ import menuServices from "../../../services/menu.service";
 import { useQuery } from "@tanstack/react-query";
 import { Navigation, Pagination, A11y } from "swiper/modules";
 import { Swiper, SwiperSlide } from 'swiper/react';
+import { cn } from "../../../utils/cn";
 
 const FeaturedMenu = () => {    
     const requestParams = {
@@ -43,7 +44,7 @@ const FeaturedMenu = () => {
                         <div
                             className="grid grid-cols-6 md:grid-cols-12 gap-6 md:gap-4 items-center justify-center"
                         >
-                            <div className="relative col-span-6 md:col-span-4">
+                            <div className="relative col-span-6 md:col-span-4 p-2">
                                 <Skeleton className="rounded-lg">
                                     <Image
                                         className="w-full object-cover h-[230px] lg:h-[300px]"
@@ -86,7 +87,7 @@ const FeaturedMenu = () => {
                 // onSlideChange={() => console.log('slide change')}
                 // onClick={(swiper) =>{ console.log(swiper)}}
             >
-                {!isLoading && menus.map((item: IMenu)=>(
+                {!isLoading && menus.map((item: IMenu, i: number)=>(
                     <SwiperSlide>
                         <Card
                             key={item.id}
@@ -96,11 +97,19 @@ const FeaturedMenu = () => {
                             className="w-full"
                         >
                             <CardBody
-                            className="p-0 bg-stone-100">
+                                // className="p-0 bg-gradient-to-r from-sky-600 to-teal-400"
+                                className={cn("p-0 bg-gradient-to-r", {
+                                    "from-teal-400 to-sky-600": i === 0,
+                                    "from-pink-600 to-purple-950": i === 1,
+                                    "from-cyan-900 via-cyan-950 to-gray-800": i === 2,
+                                    "from-pink-700 via-gray-500 to-cyan-600": i === 3,
+                                    "from-red-500 to-gray-900": i === 4,
+                                })}
+                            >
                                 <div
                                     className="grid grid-cols-6 md:grid-cols-12 gap-6 md:gap-4 items-center justify-center"
                                 >
-                                    <div className="relative col-span-6 md:col-span-4">
+                                    <div className="relative col-span-6 md:col-span-4 p-2">
                                         <Image
                                             alt={item.name}
                                             className="w-full object-cover h-[230px] lg:h-[300px]"
@@ -110,11 +119,11 @@ const FeaturedMenu = () => {
                                             width="100%"
                                         />
                                     </div>
-                                    <div className="flex flex-col col-span-6 md:col-span-8 gap-3 ml-5 mb-10 lg:mb-0">
-                                        <h1 className="text-2xl font-bold">{item.name}</h1>
-                                        <p className="text-default-500 text-lg lg:text-xl">
+                                    <div className="flex flex-col col-span-6 md:col-span-8 gap-3 ml-5 mb-10 lg:mb-0 text-white">
+                                        <h1 className="text-3xl font-bold">{item.name}</h1>
+                                        <p className=" text-lg lg:text-2xl">
                                             Only&nbsp;
-                                            <span className="text-teal-600 font-bold">${item.price}</span>
+                                            <span className=" font-bold">${item.price}</span>
                                         </p>
                                         <p className="text-lg">
                                             {item.description}
