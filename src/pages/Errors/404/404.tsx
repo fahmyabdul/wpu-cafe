74cfe65299c5912b5b6d60b1da608491ca5b1216
Fahmy Abdul <firstfahmyabdul@gmail.com>
2025-04-06 08:48:52 +0700

    feat: minor change 404 page; change CustomSpinner to use gif for safari support

--- src/pages/Errors/404/404.tsx
+++ src/pages/Errors/404/404.tsx
@@ -3,6 +3,7 @@ import ErrorLayout from "../../../components/layouts/ErrorLayout";
 import { useEffect } from "react";
 import useThemeSwitchStore from "../../../stores/ThemeSwitchStore";
 import { HiOutlineMoon, HiOutlineSun } from "react-icons/hi2";
+import { Link } from "@heroui/link";
 
 const Page404 = () => {
     useEffect(() => {
@@ -28,7 +29,7 @@ const Page404 = () => {
             <div className="w-full md:w-2/3 flex flex-col mb-16 items-center text-center">
               <h1 className="title-font sm:text-5xl text-3xl mb-4 font-medium bg-gradient-to-r bg-clip-text from-sky-600 to-teal-400 text-transparent pb-1">Page Not Found</h1>
               <p className="-mt-2 mb-3 text-lg leading-relaxed text-gray-500 dark:text-gray-400">Woops! Where are you trying to go?!</p>
-              <a href="/" className="text-teal-600 text-md font-semibold">Back To Home</a>
+              <Link href="/" className="text-sky-600 text-md font-semibold">Back To Home</Link>
             </div>
           </div>
         </section>
