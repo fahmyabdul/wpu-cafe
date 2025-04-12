f99165d6bb9bec773808aeb9519cc67922d3a871
Fahmy Abdul <firstfahmyabdul@gmail.com>
2025-04-12 16:40:40 +0700

    fix: invoice google font set to https

--- src/pages/Orders/OrderInvoice/OrderInvoice.tsx
+++ src/pages/Orders/OrderInvoice/OrderInvoice.tsx
@@ -20,9 +20,9 @@ import cafeLogoLight from "../../../assets/cafe-logo-l.png";
 import loadingGif from "../../../assets/wpu-my.gif";
 
 Font.register({ family: "OpenSans", fonts: [
-        { src: "http://fonts.gstatic.com/s/opensans/v13/IgZJs4-7SA1XX_edsoXWog.ttf" },
-        { src: "http://fonts.gstatic.com/s/opensans/v13/MTP_ySUJH_bn48VBG8sNSi3USBnSvpkopQaUR-2r7iU.ttf", fontWeight: 600 },
-        { src: "http://fonts.gstatic.com/s/opensans/v13/k3k702ZOKiLJc3WVjuplzC3USBnSvpkopQaUR-2r7iU.ttf", fontWeight: 700 }
+        { src: "https://fonts.gstatic.com/s/opensans/v13/IgZJs4-7SA1XX_edsoXWog.ttf" },
+        { src: "https://fonts.gstatic.com/s/opensans/v13/MTP_ySUJH_bn48VBG8sNSi3USBnSvpkopQaUR-2r7iU.ttf", fontWeight: 600 },
+        { src: "https://fonts.gstatic.com/s/opensans/v13/k3k702ZOKiLJc3WVjuplzC3USBnSvpkopQaUR-2r7iU.ttf", fontWeight: 700 }
     ]
 });
 
